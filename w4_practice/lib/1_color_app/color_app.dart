f48a09022b1a131f2b90d6c988cebfbe0b7cdb7f
Chey Rotana <rotana.chey@student.cadt.edu.kh>
2026-02-19 00:08:49 +0700

    W4 - Color App - Remove props and state drilling

--- w4_practice/lib/1_color_app/color_app.dart
+++ w4_practice/lib/1_color_app/color_app.dart
@@ -11,6 +11,23 @@ void main() {
 
 enum CardType { red, blue }
 
+class ColorService extends ChangeNotifier {
+  int redTapCount = 0;
+  int blueTapCount = 0;
+
+  void incrementRedTapCount() {
+    redTapCount++;
+    notifyListeners();
+  }
+
+  void incrementBlueTapCount() {
+    blueTapCount++;
+    notifyListeners();
+  }
+}
+
+ColorService colorService = ColorService();
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -20,36 +37,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
-  int redTapCount = 0;
-  int blueTapCount = 0;
-
-  void _incrementRedTapCount() {
-    setState(() {
-      redTapCount++;
-    });
-  }
-
-  void _incrementBlueTapCount() {
-    setState(() {
-      blueTapCount++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _currentIndex == 0
-              ? ColorTapsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-                onRedTap: _incrementRedTapCount,
-                onBlueTap: _incrementBlueTapCount,
-              )
-              : StatisticsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-              ),
+      body: ListenableBuilder(
+        listenable: colorService,
+        builder: (context, child) {
+          return _currentIndex == 0 ? ColorTapsScreen() : StatisticsScreen();
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -73,30 +70,15 @@ class _HomeState extends State<Home> {
 }
 
 class ColorTapsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-  final VoidCallback onRedTap;
-  final VoidCallback onBlueTap;
-
-  const ColorTapsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-    required this.onRedTap,
-    required this.onBlueTap,
-  });
+  const ColorTapsScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Color Taps')),
       body: Column(
         children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(
-            type: CardType.blue,
-            tapCount: blueTapCount,
-            onTap: onBlueTap,
-          ),
+          ColorTap(type: CardType.red),
+          ColorTap(type: CardType.blue),
         ],
       ),
     );
@@ -105,17 +87,22 @@ class ColorTapsScreen extends StatelessWidget {
 
 class ColorTap extends StatelessWidget {
   final CardType type;
-  final int tapCount;
-  final VoidCallback onTap;
 
-  const ColorTap({
-    super.key,
-    required this.type,
-    required this.tapCount,
-    required this.onTap,
-  });
+  const ColorTap({super.key, required this.type});
 
   Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
+
+  int get tapCount => type == CardType.red
+      ? colorService.redTapCount
+      : colorService.blueTapCount;
+
+  void onTap() {
+    if (type == CardType.red) {
+      colorService.incrementRedTapCount();
+    } else {
+      colorService.incrementBlueTapCount();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -141,14 +128,7 @@ class ColorTap extends StatelessWidget {
 }
 
 class StatisticsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-
-  const StatisticsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-  });
+  const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -158,8 +138,14 @@ class StatisticsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
+            Text(
+              'Red Taps: ${colorService.redTapCount}',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              'Blue Taps: ${colorService.blueTapCount}',
+              style: TextStyle(fontSize: 24),
+            ),
           ],
         ),
       ),
